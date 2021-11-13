c274c13f1e2678fd3655c394cd14e231b11fc51b
Axel Berggraf Egenæs <69816934+axelberggraf@users.noreply.github.com>
2021-11-13 17:50:13 +0100

    Add files via upload

---
+++ layers.js
@@ -0,0 +1,48 @@
+function hideRed() {
+  var checkbox = document.getElementById("redLayer");
+  if (checkbox.checked != true) {
+    redImg.classList.add("hidden");
+  } else {
+    redImg.classList.remove("hidden");
+  }
+}
+
+function hideGreen() {
+  var checkbox = document.getElementById("greenLayer");
+  if (checkbox.checked != true) {
+    greenImg.classList.add("hidden");
+  } else {
+    greenImg.classList.remove("hidden");
+  }
+}
+
+function hideBlue() {
+  var checkbox = document.getElementById("blueLayer");
+  if (checkbox.checked != true) {
+    blueImg.classList.add("hidden");
+  } else {
+    blueImg.classList.remove("hidden");
+  }
+}
+
+function enablePerspective() {
+  perspectiveEnabled = !perspectiveEnabled;
+  document.getElementById("perspectiveButton").classList.toggle("pressed");
+  document.getElementById("card").classList.toggle("perspective");
+}
+
+NiceSelect.bind(document.getElementById("treatment"), {
+  searchable: false,
+});
+
+NiceSelect.bind(document.getElementById("red"), {
+  searchable: false,
+});
+
+NiceSelect.bind(document.getElementById("green"), {
+  searchable: false,
+});
+
+NiceSelect.bind(document.getElementById("blue"), {
+  searchable: false,
+});
